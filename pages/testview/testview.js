f4a8cfffa56340983d330e3cf39bbf187dfbc4c6
unknown <unknown@example.com>
2020-01-06 18:17:33 +0800

    2020-1-6

--- pages/testview/testview.js
+++ pages/testview/testview.js
@@ -3,7 +3,12 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    staffA: { firstName: 'Hulk', lastName: 'Hu' },
+    staffB: { firstName: 'Shang', lastName: 'You' },
+    staffC: { firstName: 'Gideon', lastName: 'Lin' },
+    logs: [],
+    a: 1,
+    b: 2
   },
   onLoad: function () {
     this.setData({
